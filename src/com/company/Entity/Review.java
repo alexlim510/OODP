11af3f2efc705d7cd4fc65bd71df827c4f6ccc35
alexlim56 <42902719+alexlim56@users.noreply.github.com>
2019-11-15 16:25:36 +0800

    alex update

--- src/com/company/Entity/Review.java
+++ src/com/company/Entity/Review.java
@@ -13,19 +13,16 @@ public class Review implements Serializable{
    private LocalDateTime reviewDateTime;
    private String content;
    private Customer customer;
-   private Movie movie;
    private float rating;
 
    /**
     * Creates a new Review with the movie, customer, content and reviewDatetime.
-    * @param movie This Review's movie
     * @param customer This Review's customer
     * @param content This Review's content
     * @param rating This Review's rating
     * @param reviewDateTime This Review's DateTime
     */
-   public Review(Movie movie, Customer customer, String content, float rating, LocalDateTime reviewDateTime) {
-      this.movie = movie;
+   public Review(Customer customer, String content, float rating, LocalDateTime reviewDateTime) {
       this.customer = customer;
       this.content = content;
       this.reviewDateTime = reviewDateTime;
@@ -71,20 +68,6 @@ public class Review implements Serializable{
       this.customer = customer;
    }
 
-   /**
-    * Gets the movie of this Review.
-    * @return This Review's movie.
-    */
-   public Movie getMovie() {
-      return this.movie;
-   }
-   /**
-    * Changes the movie of this Review.
-    * @param movie This Review's new movie.
-    */
-   public void setMovie(Movie movie) {
-      this.movie = movie;
-   }
    /**
     * Changes the DateTime of this Review to the current DateTime.
     */
